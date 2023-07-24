02cd3af7452b5752e432ceabc920214659a112d5
Muhammad Haitsam <54610524+mhaitsam18@users.noreply.github.com>
2023-07-25 00:54:06 +0700

    Update index.php

--- application/views/user/index.php
+++ application/views/user/index.php
@@ -21,7 +21,7 @@
 	</div>
 
 	<div class="row">
-		<div class="row" style="margin-top: 300px;">
+		<div class="row">
 			<div class="col-12 col-xl-12 stretch-card">
 				<div class="row flex-grow-1">
 					<div class="col-md-12 grid-margin stretch-card">
