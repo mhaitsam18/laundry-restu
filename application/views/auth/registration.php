ae73d6b27dd1fe4b9b8e34760813ce6bdfb24b4c
Muhammad Haitsam <54610524+mhaitsam18@users.noreply.github.com>
2023-07-25 01:01:05 +0700

    Update registration.php

--- application/views/auth/registration.php
+++ application/views/auth/registration.php
@@ -1,12 +1,7 @@
 <div class="col-md-8 col-xl-6 mx-auto">
     <div class="card">
         <div class="row">
-            <div class="col-md-4 pe-md-0">
-                <div class="auth-side-wrapper">
-                    <img src="<?= base_url('assets/img/app/mesin-cuci.jpg') ?>" class="w-100" alt="">
-                </div>
-            </div>
-            <div class="col-md-8 ps-md-0">
+            <div class="col-md-12 ps-md-0">
                 <div class="auth-form-wrapper px-4 py-5">
                     <a href="#" class="noble-ui-logo d-block mb-2"><?= app_brand() ?></a>
                     <h5 class="text-muted fw-normal mb-4">Buat Akun Gratis!!</h5>
